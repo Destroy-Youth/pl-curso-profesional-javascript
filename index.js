98ee03f52d2f0fa6de4da5b0afa03a8a25ba4714
Ivan H. Equihua <luis.herrerae@outlook.com>
2021-01-22 08:47:05 -0600

    import fix

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
+import MediaPlayer from './src/MediaPlayer.js'
+
 const video = document.querySelector('video')
 const button = document.querySelector('button')
-
-class MediaPlayer {
-  constructor({ mediaElement }) {
-    this.media = mediaElement
-  }
-
-  play() {
-    if (this.media.paused) this.media.play()
-    else this.media.pause()
-  }
-}
 
 const player = new MediaPlayer({ mediaElement: video })
 
