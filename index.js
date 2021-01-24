6b2d2009eb14f2e7bdbdecef2c13e727816b9e3a
Ivan H. Equihua <luis.herrerae@outlook.com>
2021-01-24 10:03:17 -0600

    Autoplay plugin added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import MediaPlayer from './src/MediaPlayer.js'
+import AutoPlay from './src/plugins/AutoPlay.js'
 
 const video = document.querySelector('video')
 const button = document.querySelector('button')
 
-const player = new MediaPlayer({ mediaElement: video })
+const player = new MediaPlayer({
+  mediaElement: video,
+  plugins: [new AutoPlay()],
+})
 
 button.onclick = () => {
   player.play()
--- src/MediaPlayer.js
+++ src/MediaPlayer.js
@@ -1,11 +1,30 @@
 class MediaPlayer {
-  constructor({ mediaElement }) {
+  constructor({ mediaElement, plugins = [] }) {
     this.media = mediaElement
+    this.plugins = plugins
+
+    this.initPlugins()
+  }
+
+  initPlugins() {
+    console.log('init plugins')
+    this.plugins.forEach(plugin => {
+      console.log('init plugins', plugin)
+      plugin.run(this)
+    })
   }
 
   play() {
     if (this.media.paused) this.media.play()
     else this.media.pause()
+  }
+
+  mute() {
+    this.media.muted = true
+  }
+
+  unmute() {
+    this.media.mute = false
   }
 }
 
