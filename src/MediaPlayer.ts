6900be3151a8f5f638321d6e13570f89dec4762d
Ivan H. Equihua <luis.herrerae@outlook.com>
2021-02-03 22:50:15 -0600

    Ads plugin style and render

--- src/MediaPlayer.ts
+++ src/MediaPlayer.ts
@@ -3,12 +3,21 @@ import IPlugin from './plugins/IPlugin'
 class MediaPlayer {
   media: HTMLMediaElement
   plugins: IPlugin[]
+  container: HTMLElement
 
   constructor({ mediaElement, plugins = [] }) {
     this.media = mediaElement
     this.plugins = plugins
 
+    this.initPlayer()
     this.initPlugins()
+  }
+
+  initPlayer() {
+    this.container = document.createElement('div')
+    this.container.style.position = 'relative'
+    this.media.parentNode.insertBefore(this.container, this.media)
+    this.container.appendChild(this.media)
   }
 
   private initPlugins() {
--- src/plugins/Ads.ts
+++ src/plugins/Ads.ts
@@ -113,14 +113,17 @@ class AdsPlugin implements IPlugin {
   private player: MediaPlayer
   private media: HTMLMediaElement
   private currentAd: Ad
+  private adsContainer: HTMLElement
 
   constructor() {
     this.ads = Ads.getInstance()
+    this.adsContainer = document.createElement('div')
     this.handleTimeUpdate = this.handleTimeUpdate.bind(this)
   }
 
   run(player: MediaPlayer): void {
     this.player = player
+    this.player.container.appendChild(this.adsContainer)
     this.media = this.player.media
     this.media.addEventListener('timeupdate', this.handleTimeUpdate)
   }
@@ -136,9 +139,24 @@ class AdsPlugin implements IPlugin {
     if (this.currentAd) {
       return
     }
-    const ad = this.ads.getAd()
-    this.currentAd = ad
-    console.log(ad)
+
+    this.currentAd = this.ads.getAd()
+    this.adsContainer.innerHTML = `
+    <div class="ads">
+      <a  class="ads__link" href="${this.currentAd.url}" target="_blank">
+        <img class="ads__img" src="${this.currentAd.imageUrl}" />
+        <div class="ads__info">
+          <h5 class="ads__title">${this.currentAd.title}</h5>
+          <p class="ads__body">${this.currentAd.body}</p>
+        </div>
+      </a>
+    </div>
+    `
+
+    setTimeout(() => {
+      this.currentAd = null
+      this.adsContainer.innerHTML = ''
+    }, 5000)
   }
 }
 
